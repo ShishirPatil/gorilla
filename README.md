816f60c0f14f277ccee619d6e88cf1bfe46ce485
Shishir Patil <shishirpatil@berkeley.edu>
2023-05-25 02:28:11 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,64 @@
+# Gorilla: Large Language Model Connected with Massive APIs
+By Shishir G. Patil, Tianjun Zhang, Xin Wang, and Joseph E. Gonzalez  ([Project Website](https://shishirpatil.github.io/gorilla/))
+
+![](https://github.com/ShishirPatil/gorilla/blob/gh-pages/assets/img/logo.png)
+
+_See the paper!_ [https://arxiv.org/abs/2305.15334](https://arxiv.org/abs/2305.15334)
+
+_Join our Discord!_ [https://discord.gg/3apqwwME](https://discord.gg/3apqwwME) 
+
+`Gorilla` enables LLMs to use tools by invoking APIs. Given a natural language query, Gorilla comes up with the semantically- and syntactically- correct API to invoke. With Gorilla, we are the first to demonstrate how to use LLMs to invoke 1,600+ (and growing) API calls accurately while reducing hallucination. Join us, as we try to build the API store for LLMs! Hop on our Discord, or open a PR, or email us if you would like to have your API incorporated as well.
+
+## Repository Organization
+
+We include the `APIBench` dataset created by self-instruct in `data/apibench`. All the 1640 API documentation is in `data/api`. We convert this into a LLM-friendly chat format for evaluation, and the questions are in `eval/eval-data/questions`, and the corresponding responces are in `eval/eval-data/responses`.  We have also included the evaluation scripts are in `eval/eval-scripts`. This would be entirely sufficient to train Gorilla yourself, and reproduce our results.
+Additionally, to make it more accessible, we will also release the model weights soon! Eitherways, if you run into any issues please feel free to reach out to us either through Discord or email or raise a Github issue.
+
+## Abstract
+
+From our [paper](https://arxiv.org/abs/2305.15334):
+
+```text
+Large Language Models (LLMs) have seen an impressive wave of advances recently, with models 
+now excelling in a variety of tasks, such as mathematical reasoning and program synthesis. 
+However, their potential to effectively use tools via API calls remains unfulfilled. 
+This is a challenging task even for today's state-of-the-art LLMs such as GPT-4, largely 
+due to their inability to generate accurate input arguments and their tendency to hallucinate 
+the wrong usage of an API call. We release Gorilla, a finetuned LLaMA-based model that surpasses 
+the performance of GPT-4 on writing API calls. When combined with a document retriever, 
+Gorilla demonstrates a strong capability to adapt to test-time document changes, enabling 
+flexible user updates or version changes. It also substantially mitigates the issue of 
+hallucination, commonly encountered when prompting LLMs directly. To evaluate the model's 
+ability, we introduce APIBench, a comprehensive dataset consisting of HuggingFace, TorchHub, 
+and TensorHub APIs. The successful integration of the retrieval system with Gorilla demonstrates 
+the potential for LLMs to use tools more accurately, keep up with frequently updated documentation, 
+and consequently increase the reliability and applicability of their outputs. 
+```
+
+## FAQ(s)
+
+Can we use Gorilla with Langchain, Toolformer, AutoGPT etc?
+
+Absolutely! You've highlighted a great aspect of our tools. Gorilla is  an  end-to-end model, specifically tailored to serve correct API calls without requiring any additional coding. It's designed to work as part of a wider ecosystem and can be flexibly integrated with other tools.
+
+Langchain, is a versatile developer tool. Its "agents" can efficiently swap in any LLM, Gorilla included, making it a highly adaptable solution for various needs.
+
+AutoGPT, on the other hand, concentrates on the art of prompting GPT series models. It's worth noting that Gorilla, as a fully fine-tuned model, consistently shows remarkable accuracy, and lowers hallucination, outperforming GPT-4 in making specific API calls.
+
+Now, when it comes to ToolFormer, Toolformer zeroes in on a select set of tools, providing specialized functionalities. Gorilla, in contrast, has the capacity to manage thousands of API calls, offering a broader coverage over a more extensive range of tools.
+
+The beauty of these tools truly shines when they collaborate, complementing each other's strengths and capabilities to create an even more powerful and comprehensive solution. This is where your contribution can make a difference. We enthusiastically welcome any inputs to further refine and enhance these tools. 
+
+## Citation
+```text
+@article{patil2023gorilla,
+  title={Gorilla: Large Language Model Connected with Massive APIs},
+  author={Shishir G. Patil and Tianjun Zhang and Xin Wang and Joseph E. Gonzalez},
+  year={2023},
+  journal={arXiv preprint arXiv:2305.15334},
+} 
+```
+
+
+
+
---
+++ eval/eval-scripts/ast_eval_hf.py
@@ -0,0 +1,172 @@
+import argparse
+import json 
+from codebleu.parser import DFG_python,DFG_java,DFG_ruby,DFG_go,DFG_php,DFG_javascript,DFG_csharp
+from codebleu.parser import (remove_comments_and_docstrings,
+                   tree_to_token_index,
+                   index_to_code_token,
+                   tree_to_variable_index)
+from tree_sitter import Language, Parser
+
+dfg_function={
+    'python':DFG_python,
+    'java':DFG_java,
+    'ruby':DFG_ruby,
+    'go':DFG_go,
+    'php':DFG_php,
+    'javascript':DFG_javascript,
+    'c_sharp':DFG_csharp,
+}
+
+# Get all the subtrees given a root_node
+def get_all_sub_trees(root_node):
+    node_stack = []
+    sub_tree_sexp_list = []
+    depth = 1
+    text = root_node.text
+    node_stack.append([root_node, depth])
+    while len(node_stack) != 0:
+        cur_node, cur_depth = node_stack.pop()
+        if cur_node.child_count > 0:
+            sub_tree_sexp_list.append([cur_node.sexp(), cur_depth, cur_node, cur_node.children[0].text])
+        else:
+            sub_tree_sexp_list.append([cur_node.sexp(), cur_depth, cur_node, None])
+        for child_node in cur_node.children:
+            if len(child_node.children) != 0:
+                depth = cur_depth + 1
+                node_stack.append([child_node, depth])
+    return sub_tree_sexp_list
+
+# Parse the program into AST trees
+def ast_parse(candidate, lang='python'):
+    LANGUAGE = Language('codebleu/parser/my-languages.so', lang)
+    parser = Parser()
+    parser.set_language(LANGUAGE)
+    
+    candidate_tree = parser.parse(bytes(candidate,'utf8')).root_node
+    return candidate_tree
+
+# Get all the arguments in the ast tree
+def get_args(node):
+    if node.child_count == 0:
+        return []
+    args_list = []
+    for child in node.children[0].children[0].children[1].children:
+        if "=" in child.text.decode():
+            args_list.append(child.children[2].text)
+        elif child.text.decode() != "(" and child.text.decode() != ")" and child.text.decode() != ",":
+            args_list.append(child.text)
+    return args_list
+
+# Check if there is an api match 
+def ast_check(candidate_subtree_list, base_tree_list):
+    for idx, base_tree in enumerate(base_tree_list):
+        if base_tree.children[0].children[0].child_count == 0:
+            continue
+        api_name = base_tree.children[0].children[0].children[0].text
+        for candidate_tree in candidate_subtree_list:
+            if candidate_tree[3] == api_name:
+                break
+        # Now we have a sub-tree
+        candidate_tree = candidate_tree[2]
+        args_list = get_args(base_tree)
+        if len(args_list) == 0:
+            continue
+        ast_match = True
+        for arg in args_list:
+            if arg.decode().lstrip("'").rstrip("'") not in candidate_tree.text.decode():
+                ast_match = False
+                break
+        if ast_match:
+            return idx
+    return -1
+
+# Parse the dataset
+def parse_dataset(args):
+    # Read the api datasest
+    api_database = []
+    with open(args.api_dataset, 'r') as f:
+        for line in f:
+            api_database.append(json.loads(line))
+
+    # Read the question answer pair datasest
+    qa_pairs = []
+    with open(args.apibench, 'r') as f:
+        for line in f:
+            qa_pairs.append(json.loads(line)["api_data"])
+    
+    # Read the language model response datasest
+    llm_responses = []
+    with open(args.llm_responses, 'r') as f:
+        for line in f:
+            llm_responses.append(json.loads(line))
+
+    # Parse all apis to ast trees
+    ast_database = []
+    for data in api_database:
+        ast_tree = ast_parse(data['api_call'])
+        ast_database.append(ast_tree)
+
+    return api_database, qa_pairs, llm_responses, ast_database
+
+def main(args):
+    # Read datsets
+    api_database, qa_pairs, llm_responses, ast_database = parse_dataset(args)
+
+    # Check correctness
+    total_correct = 0
+    total_hallucination = 0
+    for idx, response in enumerate(llm_responses):
+        try:
+            output = response['text']
+        except:
+            print('Error: cannot parse line ', idx)
+            continue
+
+        # Index the "api_call" domain
+        output = output.split("api_call")
+        if len(output) == 1:
+            # print('Error: line ', idx, ' is not the right format')
+            # continue
+            api_call = output[0]
+        else:
+            # Parse the output
+            output = output[1].split("api_provider")[0]
+            if ":" not in output:
+                start = 0
+            else:
+                start = output.index(":")
+            if ")" not in output:
+                end = -2
+            else:
+                end = output.rindex(")")
+            api_call = output[start+2:end+1]
+
+
+        # Parse the api_call into AST tree
+        ast_tree = ast_parse(api_call)
+        # Search for a subtree
+        ast_subtree_list = get_all_sub_trees(ast_tree)
+        # Check which ast tree is matching
+        database_index = ast_check(ast_subtree_list, ast_database)
+        # We cannot index this ast in our database
+        if database_index == -1: 
+            total_hallucination += 1
+            continue
+        # We index our reference api_call
+        ref_api_call = api_database[database_index]
+        # Check for functionality
+        if ref_api_call['domain'] == qa_pairs[response['question_id'] - 1]['domain']:
+            total_correct += 1
+        else:
+            pass
+
+    print('Final Functionality accuracy: ', total_correct / len(llm_responses))
+    print('Final hallucination: ', total_hallucination/len(llm_responses))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--api_dataset", type=str, default=None, help="path to your api dataset")
+    parser.add_argument("--apibench", type=str, default=None, help="path to your apibench dataset including the question and answer pairs")
+    parser.add_argument("--llm_responses", type=str, default=None, help="path to the language model responses")
+    args = parser.parse_args()
+    main(args)
