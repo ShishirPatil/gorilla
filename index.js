95bab0fc37f549ae799b4cd9183f6fbb2431f9ca
Huanzhi Mao <huanzhimao@gmail.com>
2024-02-26 17:08:23 -0800

    add back google sheet for contact form. the server is now working.

--- index.js
+++ index.js
@@ -95,22 +95,22 @@ document.getElementById('report-issue-btn').addEventListener('click', function (
 
 
 
-// const scriptURL = 'https://script.google.com/macros/s/AKfycbxdAKyEA36HA_p0k3KwMzMigxgFCZ1XegRBPfjgxlNaOK2CsOnP9hrEV_6V1ARCAJw3vw/exec'
-// const form = document.forms['submit-to-google-sheet']
-// const msg = document.getElementById("msg")
-// form.addEventListener('submit', e => {
-//     e.preventDefault()  // Prevents default refresh by the browser
-//     fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-//         .then(response => {
-//             console.log('Success!', response);
-//             msg.innerHTML = "<span style='color: black;'>Message Sent Successfully!</span>";
-//             setTimeout(function () {
-//                 msg.innerHTML = ""
-//             }, 5000)
-//             // form.reset()
-//         })
-//         .catch(error => console.error('Error!', error.message))
-// })
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxdAKyEA36HA_p0k3KwMzMigxgFCZ1XegRBPfjgxlNaOK2CsOnP9hrEV_6V1ARCAJw3vw/exec'
+const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+form.addEventListener('submit', e => {
+    e.preventDefault()  // Prevents default refresh by the browser
+    fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+        .then(response => {
+            console.log('Success!', response);
+            msg.innerHTML = "<span style='color: black;'>Message Sent Successfully!</span>";
+            setTimeout(function () {
+                msg.innerHTML = ""
+            }, 5000)
+            // form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
 
 
 
