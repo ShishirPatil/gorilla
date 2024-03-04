1151688f4d38301c6803fd0be7bf46f02ccffdc9
Fanjia Yan <fanjiayan@berkeley.edu>
2024-03-04 13:38:00 -0800

    fix input file None blocking issue

--- berkeley-function-call-leaderboard/openfunctions_executable_checker.py
+++ berkeley-function-call-leaderboard/openfunctions_executable_checker.py
@@ -123,15 +123,15 @@ for i in tqdm(range(len(result_data))):
         except:     
             total += 1
             continue
-    if input_file is not None and "gorilla" in input_file:
+    elif input_file is not None and "gorilla" in input_file:
         result = result_data[i]["text"] 
         print(result)
-    if "gorilla-openfunctions-v2" in model_name:
+    elif "gorilla-openfunctions-v2" in model_name:
         result = result_data[i]["result"]
         regex = r"\w+\([^()]*\)"
         # Find all matches of function calls
         result = re.findall(regex, result)
-    if input_file is not None and "deepseek" in input_file:
+    elif input_file is not None and "deepseek" in input_file:
         result_data[i]["text"] = result_data[i]["text"].replace("\n","")
         try:
             pattern = r"```python(.*?)```"
@@ -188,7 +188,7 @@ for i in tqdm(range(len(result_data))):
         result = func.split("),")
         result = [r + ")" for r in result]
         result[-1] = result[-1][:-1]
-    elif "glaiveai" in model_name or "glaiveai" in input_file:
+    elif "glaiveai" in model_name or (input_file is not None and "glaiveai" in input_file):
         try:
             result_data[i]["text"] = json.loads(result_data[i]["text"].split("<functioncall>")[1].replace("\'{","{").replace("\'}","}"))
             result = result_data[i]["text"]["name"] + "(" + ",".join([f"{k}={v}" for k,v in result_data[i]["text"]["arguments"].items()]) + ")"
@@ -202,6 +202,9 @@ for i in tqdm(range(len(result_data))):
                 continue
             total += 1
             continue
+    else:
+        print("Model not supported yet for executable result.")
+        exit()
     if type(result) == str:
             result = [result]
     exec_dict = {}
