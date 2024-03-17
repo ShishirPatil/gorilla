4b5e7a4e96f5ca1df8aaea0fade0d576ebed3f2a
Mokhalad <mjayalju12234@gmail.com>
2024-03-17 12:24:55 -0700

    bringing back settings.json and fixing apizoo navbar

--- apizoo/app/src/components/ApiZoo.vue
+++ apizoo/app/src/components/ApiZoo.vue
@@ -5,9 +5,9 @@
       <span class="nav-separator">|</span>
       <a href="../blog.html">Blog</a>
       <span class="nav-separator">|</span>
-      <a href="apizoo/">API Zoo Index</a>
+      <a href="/apizoo">API Zoo Index</a>
       <span class="nav-separator">|</span>
-      <a href="../../../../add-api/build">Add Your API</a>
+      <a href="/add-api/build">Add Your API</a>
   </div>
   <div class="api-zoo-container">
     <h1>🦍 Gorilla: API Zoo Index 🚀</h1>
