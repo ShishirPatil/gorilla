7265f62cf39899580a94cec4b2f7b557e4a5d32d
Mokhalad <mjayalju12234@gmail.com>
2024-03-25 22:58:07 -0400

    update to incorportate hosted backend

--- add-api/client/vite.config.ts
+++ add-api/client/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://34.133.163.39/addapi/',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
