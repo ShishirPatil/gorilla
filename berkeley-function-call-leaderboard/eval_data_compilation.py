49718c37f9f82f94b8874d139ce3136f25478f7e
Huanzhi Mao <huanzhimao@gmail.com>
2024-04-05 01:09:44 -0700

    update support for oss evaluation

--- berkeley-function-call-leaderboard/eval_data_compilation.py
+++ berkeley-function-call-leaderboard/eval_data_compilation.py
@@ -1,19 +1,37 @@
-import os
 import json
+
 data = []
 """
     Compile evaluation data into a single file
 """
-for filename in os.listdir("./data"):
-    if "gorilla" in filename:
-        with open(f"./data/{filename}", "r") as file:
-            for line in file:
-                item = json.loads(line)
-                name = filename.replace("gorilla_openfunctions_v1_test_","").replace(".json","")
-                item["question_type"] = name
-                data.append(item)
-with open("./eval_data_total.json", "a+") as file:
+
+test_files = [
+    "executable_parallel_function",
+    "parallel_multiple_function",
+    "executable_simple",
+    "rest",
+    "sql",
+    "parallel_function",
+    "chatable",
+    "java",
+    "javascript",
+    "executable_multiple_function",
+    "simple",
+    "relevance",
+    "executable_parallel_multiple_function",
+    "multiple_function",
+]
+
+for test_name in test_files:
+    with open(f"./data/gorilla_openfunctions_v1_test_{test_name}.json", "r") as file:
+        for line in file:
+            item = json.loads(line)
+            item["question_type"] = test_name
+            data.append(item)
+
+with open("./eval_data_total.json", "w") as file:
     for item in data:
         file.write(json.dumps(item))
         file.write("\n")
+
 print("Data successfully compiled into eval_data_total.json 🦍")
