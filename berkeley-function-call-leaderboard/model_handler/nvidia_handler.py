19dae66ad78d0a6eec4f4cd9c534a98504c689e5
Fanjia Yan <fanjiayan@berkeley.edu>
2024-04-27 18:09:45 -0700

    change nvidia handlers parent

--- berkeley-function-call-leaderboard/model_handler/nvidia_handler.py
+++ berkeley-function-call-leaderboard/model_handler/nvidia_handler.py
@@ -1,6 +1,6 @@
 import time,os,json
 from openai import OpenAI
-from model_handler.gpt_handler import OpenAIHandler
+from model_handler.handler import BaseHandler
 from model_handler.model_style import ModelStyle
 from model_handler.utils import (
     augment_prompt_by_languge,
@@ -11,7 +11,7 @@ from model_handler.constant import (
     SYSTEM_PROMPT_FOR_CHAT_MODEL,
 )
 
-class NvidiaHandler(OpenAIHandler):
+class NvidiaHandler(BaseHandler):
     def __init__(self, model_name, temperature=0.7, top_p=1, max_tokens=1000) -> None:
         self.model_name = model_name
         self.temperature = temperature
