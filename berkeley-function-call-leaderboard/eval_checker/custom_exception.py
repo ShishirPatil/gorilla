ffb5125f4081ef4445d8a0ace383059af554a980
iamskeole <214513857+iamskeole@users.noreply.github.com>
2024-07-07 14:55:18 -0700

    [BFCL] Sanity check is now optional (#496)
    1. Sanity check is now optional. Further if the checks fail, we catch
    that and then continue the evaluation. That way if an API is down, it
    doesn't hinder other evaluations especially AST evaluations.
    2. Update README.
    
    Close #486 
    
    This PR does NOT update the Leaderboard values.
    
    ---------
    
    Co-authored-by: Huanzhi Mao <huanzhimao@gmail.com>

--- berkeley-function-call-leaderboard/eval_checker/custom_exception.py
+++ berkeley-function-call-leaderboard/eval_checker/custom_exception.py
@@ -1,10 +1,10 @@
 class NoAPIKeyError(Exception):
     def __init__(self):
-        self.message = "Please fill in the API keys in the function_credential_config.json file. If you do not provide the API keys, the executable test category results will be inaccurate."
+        self.message = "❗️Please fill in the API keys in the function_credential_config.json file. If you do not provide the API keys, the executable test category results will be inaccurate."
         super().__init__(self.message)
         
         
 class BadAPIStatusError(Exception):
-    def __init__(self, message):
-        self.message = message
-        super().__init__(self.message)
+    def __init__(self, errors, error_rate):
+        self.errors = errors
+        self.error_rate = error_rate
