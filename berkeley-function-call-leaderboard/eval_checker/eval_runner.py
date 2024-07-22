3c6e42e7bdf29615461408d267f4c239c60ba352
Huanzhi Mao <huanzhimao@gmail.com>
2024-07-21 21:17:31 -0700

    remove outdated oss_file_formatter function

--- berkeley-function-call-leaderboard/eval_checker/eval_runner.py
+++ berkeley-function-call-leaderboard/eval_checker/eval_runner.py
@@ -291,20 +291,6 @@ def runner(model_names, test_categories, api_sanity_check):
 
         model_name_escaped = model_name.replace("_", "/")
 
-        files = [
-            f
-            for f in os.listdir(subdir)
-            if os.path.isfile(os.path.join(subdir, f)) and not f.startswith(".")
-        ]
-        # Check if there is only one file and that file is 'result.json'
-        # If so, this is an OSS model result file and we need to special process it first
-        if len(files) == 1 and files[0] == "result.json":
-            result_json_file_path = os.path.join(subdir, "result.json")
-            oss_file_formatter(result_json_file_path, subdir)
-            print(
-                f"Detected OSS model: {model_name}. result.json has been split into individual test category files."
-            )
-
         # Pattern to match JSON files in this subdirectory
         json_files_pattern = os.path.join(subdir, "*.json")
 
