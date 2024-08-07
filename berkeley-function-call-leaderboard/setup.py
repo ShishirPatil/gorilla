3e0953229a452edd6e8f888f56afde2f7631257f
Devansh Amin <devanshamin97@gmail.com>
2024-08-07 18:41:07 -0400

    Move out dependencies to a separate file

--- berkeley-function-call-leaderboard/setup.py
+++ berkeley-function-call-leaderboard/setup.py
@@ -1,6 +1,10 @@
 from setuptools import setup, find_packages
 import codecs
 
+def read_requirements(filename):
+    with open(filename, 'r') as f:
+        return f.read().splitlines()
+
 with codecs.open("README.md", "r", "utf-8") as f:
     long_description = f.read()
 
@@ -15,22 +19,7 @@ setup(
     python_requires=">=3.10",
     license="Apache 2.0",
     packages=find_packages(include=["bfcl*"]),
-    install_requires=[
-        "requests",
-        "tqdm",
-        "numpy==1.26.4",
-        "pandas",
-        "huggingface_hub",
-        "pydantic>=2.8.2",
-        "python-dotenv>=1.0.1",
-        "tree_sitter==0.21.3",
-        "tree-sitter-java==0.21.0",
-        "tree-sitter-javascript==0.21.4",
-        "openai==1.35.13",
-        "mistralai==0.4.2",
-        "anthropic==0.31.1",
-        "cohere==5.5.8",
-    ],
+    install_requires=read_requirements("requirements.txt"),
     extras_require={
         "oss_eval": ["vllm==0.5.0"],
     },
