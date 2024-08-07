b73e1c2b9d90c6ff8ce17520a515155007b1b1d7
Huanzhi Mao <huanzhimao@gmail.com>
2024-08-07 16:09:06 -0700

    set default temperature value to be 0.001 for all models

--- berkeley-function-call-leaderboard/model_handler/firework_ai_handler.py
+++ berkeley-function-call-leaderboard/model_handler/firework_ai_handler.py
@@ -10,10 +10,9 @@ from openai import OpenAI
 
 
 class FireworkAIHandler(OpenAIHandler):
-    def __init__(self, model_name, temperature=0.0, top_p=1, max_tokens=1000) -> None:
+    def __init__(self, model_name, temperature=0.001, top_p=1, max_tokens=1000) -> None:
         super().__init__(model_name, temperature, top_p, max_tokens)
         self.model_style = ModelStyle.FIREWORK_AI
-        self.temperature = 0.0
 
         self.client = OpenAI(
             base_url="https://api.fireworks.ai/inference/v1",
