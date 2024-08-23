4a5c5ccd34ac21bf86a1aac7414cb2dfc786ac53
Cedric Vidal <cedric@vidal.biz>
2024-08-23 14:34:24 +0000

    Support for Azure OpenAI Keyless and Managed Identity authentication

--- raft/client_utils.py
+++ raft/client_utils.py
@@ -4,9 +4,11 @@ from langchain_openai import OpenAIEmbeddings, AzureOpenAIEmbeddings
 from openai import AzureOpenAI, OpenAI
 import logging
 from env_config import read_env_config, set_env
-from os import environ
+from os import environ, getenv
 import time
 from threading import Lock
+from azure.identity import DefaultAzureCredential, ManagedIdentityCredential
+from azure.identity import get_bearer_token_provider
 
 
 logger = logging.getLogger("client_utils")
@@ -16,10 +18,12 @@ def build_openai_client(env_prefix : str = "COMPLETION", **kwargs: Any) -> OpenA
     Build OpenAI client based on the environment variables.
     """
 
+    kwargs = _remove_empty_values(kwargs)
     env = read_env_config(env_prefix)
     with set_env(**env):
         if is_azure():
-            client = AzureOpenAI(**kwargs)
+            auth_args = _get_azure_auth_client_args()
+            client = AzureOpenAI(**auth_args, **kwargs)
         else:
             client = OpenAI(**kwargs)
         return client
@@ -29,14 +33,43 @@ def build_langchain_embeddings(**kwargs: Any) -> OpenAIEmbeddings:
     Build OpenAI embeddings client based on the environment variables.
     """
 
+    kwargs = _remove_empty_values(kwargs)
     env = read_env_config("EMBEDDING")
-
     with set_env(**env):
         if is_azure():
-            client = AzureOpenAIEmbeddings(**kwargs)
+            auth_args = _get_azure_auth_client_args()
+            client = AzureOpenAIEmbeddings(**auth_args, **kwargs)
         else:
             client = OpenAIEmbeddings(**kwargs)
         return client
+
+def _remove_empty_values(d: dict) -> dict:
+    return {k: v for k, v in d.items() if v is not None}
+
+def _get_azure_auth_client_args() -> dict:
+    """Handle Azure OpenAI Keyless, Managed Identity and Key based authentication
+    https://techcommunity.microsoft.com/t5/microsoft-developer-community/using-keyless-authentication-with-azure-openai/ba-p/4111521
+    """
+    client_args = {}
+    if getenv("AZURE_OPENAI_KEY"):
+        logger.info("Using Azure OpenAI Key based authentication")
+        client_args["api_key"] = getenv("AZURE_OPENAI_KEY")
+    else:
+        if client_id := getenv("AZURE_OPENAI_CLIENT_ID"):
+            # Authenticate using a user-assigned managed identity on Azure
+            logger.info("Using Azure OpenAI Managed Identity Keyless authentication")
+            azure_credential = ManagedIdentityCredential(client_id=client_id)
+        else:
+            # Authenticate using the default Azure credential chain
+            logger.info("Using Azure OpenAI Default Azure Credential Keyless authentication")
+            azure_credential = DefaultAzureCredential()
+
+        client_args["azure_ad_token_provider"] = get_bearer_token_provider(
+            azure_credential, "https://cognitiveservices.azure.com/.default")
+    client_args["api_version"] = getenv("AZURE_OPENAI_API_VERSION") or "2024-02-15-preview"
+    client_args["azure_endpoint"] = getenv("AZURE_OPENAI_ENDPOINT")
+    client_args["azure_deployment"] = getenv("AZURE_OPENAI_DEPLOYMENT")
+    return client_args
 
 def is_azure():
     azure = "AZURE_OPENAI_ENDPOINT" in environ or "AZURE_OPENAI_KEY" in environ or "AZURE_OPENAI_AD_TOKEN" in environ
