9b09e9c78f005802c2f6c56ffdc9b80804fdab22
BeerTai <1760083838@qq.com>
2024-09-18 16:03:37 +0800

    Update unigpt.py

--- berkeley-function-call-leaderboard/bfcl/model_handler/proprietary_model/unigpt.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/proprietary_model/unigpt.py
@@ -11,7 +11,7 @@ import os, time, json
 import requests
 import hashlib
 
-class UniGPTvllmHandler(BaseHandler):
+class UniGPTHandler(BaseHandler):
     def __init__(self, model_name, temperature=0.0, top_p=1, max_tokens=1000) -> None:
         super().__init__(model_name, temperature, top_p, max_tokens)
         self.model_style = ModelStyle.OpenAI
