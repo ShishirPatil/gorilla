70ad2cc70fba05e82a6cce9fa43d3c43b38c1527
Matthew Farrellee <matt@cs.wisc.edu>
2024-09-22 19:56:57 -0400

    add commands: test-categories, models, results, scores

--- berkeley-function-call-leaderboard/bfcl/__main__.py
+++ berkeley-function-call-leaderboard/bfcl/__main__.py
@@ -3,14 +3,24 @@ from collections import namedtuple
 import typer
 from bfcl._openfunctions_evaluation import main as openfunctions_main
 from bfcl.eval_checker import eval_runner
+from bfcl.eval_checker.eval_runner_helper import MODEL_METADATA_MAPPING
+from bfcl.eval_checker.eval_checker_constant import TEST_COLLECTION_MAPPING
 import os
+from pathlib import Path
+from datetime import datetime
+from tabulate import tabulate
+import csv
 
 
 class ExecutionOrderGroup(typer.core.TyperGroup):
     def list_commands(self, ctx):
         return [
+            "models",
+            "test-categories",
             "run",
+            "results",
             "evaluate",
+            "scores",
         ]
 
 
@@ -19,6 +29,32 @@ cli = typer.Typer(
     no_args_is_help=True,
     cls=ExecutionOrderGroup,
 )
+
+
+@cli.command()
+def test_categories():
+    """
+    List available test categories.
+    """
+    table = tabulate(
+        [(category, "\n".join(test for test in tests)) for category, tests in TEST_COLLECTION_MAPPING.items()],
+        headers=["Test category", "Test names"],
+        tablefmt="grid",
+    )
+    print(table)
+
+
+@cli.command()
+def models():
+    """
+    List available models.
+    """
+    table = tabulate(
+        [[model] for model in MODEL_METADATA_MAPPING],
+        tablefmt="plain",
+        colalign=("left",),
+    )
+    print(table)
 
 
 @cli.command()
@@ -85,6 +121,53 @@ def run(
 
 
 @cli.command()
+def results():
+    """
+    List the results available for evaluation.
+    """
+
+    def display_name(name: str):
+        """
+        Undo the / -> _ transformation if it happened.
+
+        Args:
+            name (str): The name of the model in the result directory.
+
+        Returns:
+            str: The original name of the model.
+        """
+        if name not in MODEL_METADATA_MAPPING:
+            candidate = name.replace("_", "/")
+            if candidate in MODEL_METADATA_MAPPING:
+                return candidate
+            print(f"Unknown model name: {name}")
+        return name
+
+    result_dir = Path("./result")  # todo: make this configurable
+    if not result_dir.exists():
+        print("No results available.")
+        return
+
+    results_data = []
+    for dir in result_dir.iterdir():
+        results_data.append(
+            (
+                display_name(dir.name),
+                datetime.fromtimestamp(dir.stat().st_ctime).strftime(
+                    "%Y-%m-%d %H:%M:%S"
+                ),
+            )
+        )
+    print(
+        tabulate(
+            results_data,
+            headers=["Model name", "Creation time"],
+            tablefmt="pretty",
+        )
+    )
+
+
+@cli.command()
 def evaluate(
     model: List[str] = typer.Option(..., help="A list of model names to evaluate."),
     test_category: List[str] = typer.Option(
@@ -111,6 +194,26 @@ def evaluate(
 
     eval_runner.main(model, test_category, api_sanity_check)
 
+
+@cli.command()
+def scores():
+    """
+    Display the leaderboard.
+    """
+    def truncate(text, length=22):
+        return (text[:length] + '...') if len(text) > length else text
+
+    # files = ["./score/data_non_live.csv", "./score/data_live.csv", "./score/data_combined.csv"]
+    files = ["./score/data_combined.csv"]  # todo: make ./score configurable
+    for file in files:
+        if os.path.exists(file):
+            with open(file, newline='') as csvfile:
+                reader = csv.reader(csvfile)
+                headers = [truncate(header) for header in next(reader)]  # Read the header row
+                data = [[truncate(cell) for cell in row] for row in reader]  # Read the rest of the data
+                print(tabulate(data, headers=headers, tablefmt='grid'))
+        else:
+            print(f"\nFile {file} not found.\n")
 
 if __name__ == "__main__":
     cli()
