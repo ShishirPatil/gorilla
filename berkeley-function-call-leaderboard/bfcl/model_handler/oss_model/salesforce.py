729e736ce36558411079977c7038e699d3314731
Huanzhi (Hans) Mao <huanzhimao@gmail.com>
2024-10-05 14:37:45 -0700

    suppress print statement in salesforce handler

--- berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/salesforce.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/salesforce.py
@@ -70,8 +70,8 @@ class SalesforceHandler(OSSHandler):
                     [f"{key}={repr(value)}" for key, value in arguments.items()]
                 )
                 python_format.append(f"{name}({args_str})")
-            else:
-                print(f"Invalid format: {tool_call}")
+            # else:
+                # print(f"Invalid format: {tool_call}")
 
         return python_format
 
