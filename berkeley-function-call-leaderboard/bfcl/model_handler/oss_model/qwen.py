5be4078cf8e7d3376196cd2774fe974c0bfc4a53
Huanzhi (Hans) Mao <huanzhimao@gmail.com>
2024-10-04 18:36:33 -0700

    update QwenHandler to be in sync with

--- berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/qwen.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/qwen.py
@@ -1,20 +1,17 @@
 from bfcl.model_handler.oss_model.base_oss_handler import OSSHandler
 
+
 class QwenHandler(OSSHandler):
-    def __init__(self, model_name, temperature=0.001, top_p=1, max_tokens=1000) -> None:
-        super().__init__(model_name, temperature, top_p, max_tokens)
+    def __init__(self, model_name, temperature) -> None:
+        super().__init__(model_name, temperature)
 
-    def apply_chat_template(self, prompts, function, test_category):
+    def _format_prompt(self, messages, function):
+
         formatted_prompt = ""
-        for prompt in prompts:
-            formatted_prompt += f"<|im_start|>{prompt['role']}\n{prompt['content']}<|im_end|>\n"
-        formatted_prompt += "<|im_start|>assistant\n"
-        return formatted_prompt
 
-    def inference(self, test_question, num_gpus, gpu_memory_utilization):
-        return super().inference(
-            test_question,
-            num_gpus,
-            gpu_memory_utilization,
-            format_prompt_func=self.apply_chat_template,
-        )
+        for message in messages:
+            formatted_prompt += f"<|im_start|>{message['role']}\n{message['content']}<|im_end|>\n"
+
+        formatted_prompt += "<|im_start|>assistant\n"
+
+        return formatted_prompt
