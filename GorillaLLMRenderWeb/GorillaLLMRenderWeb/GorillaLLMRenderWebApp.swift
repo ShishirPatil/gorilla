a6f461b6fad40804a110f584d7baad78f28263b1
Varun Vaidya <varun.vaidya@berkeley.edu>
2024-10-12 16:21:24 -0700

    Add SwiftUI Gorilla iOS app

---
+++ GorillaLLMRenderWeb/GorillaLLMRenderWeb/GorillaLLMRenderWebApp.swift
@@ -0,0 +1,17 @@
+//
+//  GorillaLLMRenderWebApp.swift
+//  GorillaLLMRenderWeb
+//
+//  Created by Varun Vaidya on 10/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GorillaLLMRenderWebApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
