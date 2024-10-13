7d792543145452fcfeaa6073d70b99dee7fbb686
Yujie Lu <lusane24@berkeley.edu>
2024-10-13 02:52:32 -0400

    Updated gitignore +  Craeted spotify album APIs

---
+++ goex/function/spotify_play_song.py
@@ -0,0 +1,38 @@
+import os
+import pickle
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+from spotipy.oauth2 import SpotifyClientCredentials
+
+def spotify_play_song(song_name):
+    # Load spotify credentials
+    credentials_path = './credentials/spotify/token.pickle'
+    if os.path.exists(credentials_path):
+        with open(credentials_path, 'rb') as token_file:
+            spotify_token = pickle.load(token_file)
+    else:
+        raise FileNotFoundError("Spotify token file not found.")
+    token_info = SpotifyOAuth(token=spotify_token)
+    # Initialization 
+    if not token_info:
+        print("No account found")
+        return None
+    else:
+        sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    if sp is None:
+        return None
+    # Get track uri from the song name
+    # '''searches song name and finds the most popular match and then provides the URI from that'''
+    results = sp.search(q=song_name, type='track', limit=1)
+    tracks = results['tracks']['items']
+    if tracks:
+         song = tracks[0]['uri'] 
+    else:
+        print("No tracks found for:", song_name)
+        return None
+    # Uses the URI to play the song on Spotify
+    try:
+        sp.start_playback(uris=[song])
+        print("Playing")
+    except spotipy.SpotifyException as e:
+        print("Error", e)
