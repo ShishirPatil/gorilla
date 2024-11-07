b54783627e5b1e671c06c9d8f2eb1d746411a7c2
Huanzhi (Hans) Mao <huanzhimao@gmail.com>
2024-11-06 17:17:13 -0800

    add back stop token ids

--- berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/base_oss_handler.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/base_oss_handler.py
@@ -264,12 +264,21 @@ class OSSHandler(BaseHandler):
         else:
             leftover_tokens_count = min(4096, self.max_context_length - input_token_count - 2)
 
-        api_response = self.client.completions.create(
-            model=self.model_name_huggingface,
-            temperature=self.temperature,
-            prompt=formatted_prompt,
-            max_tokens=leftover_tokens_count,
-        )
+        if hasattr(self, "stop_token_ids"):
+            api_response = self.client.completions.create(
+                model=self.model_name_huggingface,
+                temperature=self.temperature,
+                prompt=formatted_prompt,
+                max_tokens=leftover_tokens_count,
+                extra_body={"stop_token_ids": self.stop_token_ids},
+            )
+        else:
+            api_response = self.client.completions.create(
+                model=self.model_name_huggingface,
+                temperature=self.temperature,
+                prompt=formatted_prompt,
+                max_tokens=leftover_tokens_count,
+            )
 
         return api_response
 
