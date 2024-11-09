3d8e08d4fc3276c1e9f5e7a4789b5627da013b78
Huanzhi (Hans) Mao <huanzhimao@gmail.com>
2024-11-08 22:56:29 -0800

    add GOGOAGENT_API_KEY to .env file

--- berkeley-function-call-leaderboard/bfcl/model_handler/proprietary_model/gogoagent.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/proprietary_model/gogoagent.py
@@ -1,13 +1,14 @@
 import os
-from openai import OpenAI
-from bfcl.model_handler.model_style import ModelStyle
-from bfcl.model_handler.base_handler import BaseHandler
+
 from bfcl.model_handler.proprietary_model.openai import OpenAIHandler
+from openai import OpenAI
+
 
 class GoGoAgentHandler(OpenAIHandler):
     def __init__(self, model_name, temperature) -> None:
-        BaseHandler.__init__(self, model_name, temperature)
-        self.model_style = ModelStyle.OpenAI
+        super().__init__(model_name, temperature)
         self.is_fc_model = False
 
-        self.client = OpenAI(base_url="https://api.gogoagent.ai", api_key=os.getenv("GOGOAGENT_API_KEY"))
+        self.client = OpenAI(
+            base_url="https://api.gogoagent.ai", api_key=os.getenv("GOGOAGENT_API_KEY")
+        )
