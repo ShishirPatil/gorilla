8c5e4ac5354638123e6d145f8a1675aced649504
iamskeole <214513857+iamskeole@users.noreply.github.com>
2024-11-25 19:30:58 -0800

    [BFCL] Add DeepSeek-V2.5, DeepSeek-Coder-V2-Instruct-0724, DeepSeek-Coder-V2-Lite-Instruct, DeepSeek-V2-Chat-0628, DeepSeek-V2-Lite-Chat (#697)
    Add the following new models to the leaderboard:
      - `deepseek-ai/DeepSeek-V2.5`
      - `deepseek-ai/DeepSeek-Coder-V2-Instruct-0724`
      - `deepseek-ai/DeepSeek-Coder-V2-Lite-Instruct`
      - `deepseek-ai/DeepSeek-V2-Chat-0628`
      - `deepseek-ai/DeepSeek-V2-Lite-Chat`

--- berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/base_oss_handler.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/oss_model/base_oss_handler.py
@@ -52,7 +52,7 @@ class OSSHandler(BaseHandler):
         backend: str,
         include_input_log: bool,
         include_state_log: bool,
-        overwrite: bool,
+        update_mode: bool,
         result_dir=RESULT_PATH,
     ):
         """
@@ -60,9 +60,9 @@ class OSSHandler(BaseHandler):
         """
         from transformers import AutoConfig, AutoTokenizer
 
-        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name_huggingface)
+        self.tokenizer = AutoTokenizer.from_pretrained(self.model_name_huggingface, trust_remote_code=True)
 
-        config = AutoConfig.from_pretrained(self.model_name_huggingface)
+        config = AutoConfig.from_pretrained(self.model_name_huggingface, trust_remote_code=True)
         if hasattr(config, "max_position_embeddings"):
             self.max_context_length = config.max_position_embeddings
         elif self.tokenizer.model_max_length is not None:
@@ -194,10 +194,7 @@ class OSSHandler(BaseHandler):
                     for future in futures:
                         # This will wait for the task to complete, so that we are always writing in order
                         result = future.result()
-                        if overwrite:
-                            self.overwrite(result, result_dir)
-                        else:
-                            self.write(result, result_dir)
+                        self.write(result, result_dir, update_mode=update_mode)
                         pbar.update()
 
 
