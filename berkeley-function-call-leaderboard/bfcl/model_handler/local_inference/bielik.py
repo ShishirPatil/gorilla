2fc45c639b4a9a3ee187daf2197d3b8be12d4350
Huanzhi (Hans) Mao <huanzhimao@gmail.com>
2025-01-20 15:31:48 -0800

    remove AutoTokenizer import

--- berkeley-function-call-leaderboard/bfcl/model_handler/local_inference/bielik.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/local_inference/bielik.py
@@ -1,17 +1,25 @@
 from bfcl.model_handler.local_inference.base_oss_handler import OSSHandler
 from overrides import override
-from transformers import AutoTokenizer
 
 # Note: This is the handler for the Bielik in prompting mode. This model does not support function calls.
+
 
 class BielikHandler(OSSHandler):
     def __init__(self, model_name, temperature) -> None:
         super().__init__(model_name, temperature)
-        self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     @override
     def _format_prompt(self, messages, function):
-        
-        formatted_prompt = self.tokenizer.apply_chat_template(messages, add_generation_prompt=True, tokenize=False)
+        """
+        "bos_token": "<s>",
+        "chat_template": "{{bos_token}}{% for message in messages %}{{'<|im_start|>' + message['role'] + '\n' + message['content'] + '<|im_end|>' + '\n'}}{% endfor %}{% if add_generation_prompt %}{{ '<|im_start|>assistant\n' }}{% endif %}",
+        """
 
+        formatted_prompt = "<s>"
+
+        for message in messages:
+            formatted_prompt += f"<|im_start|>{message['role']}\n{message['content']}<|im_end|>\n"
+
+        formatted_prompt += f"<|im_start|>assistant\n"
+        
         return formatted_prompt
