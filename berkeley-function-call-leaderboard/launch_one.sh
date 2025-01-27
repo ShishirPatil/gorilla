7a64fbd5697d0b2a5edea283e6cd92da19349d26
kirill.fedyanin <kirill.fedyanin@gmail.com>
2025-01-27 15:28:08 +0400

    some more launch changes

--- berkeley-function-call-leaderboard/launch_one.sh
+++ berkeley-function-call-leaderboard/launch_one.sh
@@ -2,10 +2,11 @@
 #rm -rf result
 
 killall /home/ubuntu/miniconda3/envs/BFCL2/bin/python
+wait 1
 #bfcl generate --test-category=simple,parallel,multiple,parallel_multiple --model="$1" --num-gpus=4 --backend=sglang
 #bfcl evaluate --test-category=simple,parallel,multiple,parallel_multiple --model="$1"
 #bfcl generate --test-category=parallel_multiple --model="$1" --num-gpus=4 --backend=sglang
 #bfcl evaluate --test-category=parallel_multiple --model="$1"
-bfcl generate --test-category=all --model="$1" --num-gpus=4 --backend=sglang
-bfcl evaluate --test-category=all --model="$1"
+bfcl generate --test-category=exec_simple --model="$1" --num-gpus=4 --backend=sglang
+bfcl evaluate --test-category=exec_simple --model="$1"
 
