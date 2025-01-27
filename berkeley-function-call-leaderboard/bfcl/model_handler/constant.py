198ba182e4a8526f4422ae86fc965180791b9220
Huanzhi (Hans) Mao <huanzhimao@gmail.com>
2025-01-27 13:48:35 -0800

    function -> functions

--- berkeley-function-call-leaderboard/bfcl/model_handler/constant.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/constant.py
@@ -1,7 +1,7 @@
 MAXIMUM_STEP_LIMIT = 20
 
 DEFAULT_SYSTEM_PROMPT_WITHOUT_FUNC_DOC = """You are an expert in composing functions. You are given a question and a set of possible functions. Based on the question, you will need to make one or more function/tool calls to achieve the purpose.
-If none of the function can be used, point it out. If the given question lacks the parameters required by the function, also point it out.
+If none of the functions can be used, point it out. If the given question lacks the parameters required by the function, also point it out.
 You should only return the function calls in your response.
 
 If you decide to invoke any of the function(s), you MUST put it in the format of [func_name1(params_name1=params_value1, params_name2=params_value2...), func_name2(params)]
