95aaef6d7afa6ea507a0246685d8641dd1ae17e8
Huanzhi (Hans) Mao <huanzhimao@gmail.com>
2025-02-05 20:14:12 -0800

    update handler

--- berkeley-function-call-leaderboard/bfcl/model_handler/api_inference/deepseek.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/api_inference/deepseek.py
@@ -1,19 +1,17 @@
 import os
 import time
 
-from bfcl.model_handler.model_style import ModelStyle
 from bfcl.model_handler.api_inference.openai import OpenAIHandler
+from bfcl.model_handler.model_style import ModelStyle
 from bfcl.model_handler.utils import retry_with_backoff
 from openai import OpenAI, RateLimitError
 from overrides import override
 
 
-# For setup instructions, please refer to https://github.com/MeetKai/functionary for setup details.
 class DeepSeekAPIHandler(OpenAIHandler):
     def __init__(self, model_name, temperature) -> None:
         super().__init__(model_name, temperature)
         self.model_style = ModelStyle.OpenAI
-        self.is_fc_model = True
         self.client = OpenAI(
             base_url="https://api.deepseek.com", api_key=os.getenv("DEEPSEEK_API_KEY")
         )
@@ -41,16 +39,42 @@ class DeepSeekAPIHandler(OpenAIHandler):
         tools = inference_data["tools"]
         inference_data["inference_input_log"] = {"message": repr(message), "tools": tools}
 
+        # Source https://api-docs.deepseek.com/quick_start/pricing
+        # This will need to be updated if newer models are released.
+        if "DeepSeek-V3" in self.model_name:
+            api_model_name = "deepseek-chat"
+        elif "DeepSeek-R1" in self.model_name:
+            api_model_name = "deepseek-reasoner"
+        else:
+            raise ValueError(f"Model name {self.model_name} not supported by DeepSeek API")
+
         if len(tools) > 0:
             return self.generate_with_backoff(
-                # The model name is always "deepseek-chat", as per https://api-docs.deepseek.com/quick_start/pricing
-                # Note: Currently, it points to `DeepSeek-V3`
-                model="deepseek-chat",
+                model=api_model_name,
                 messages=message,
                 tools=tools,
             )
         else:
             return self.generate_with_backoff(
-                model="deepseek-chat",
+                model=api_model_name,
                 messages=message,
             )
+
+    @override
+    def _query_prompting(self, inference_data: dict):
+        message: list[dict] = inference_data["message"]
+        inference_data["inference_input_log"] = {"message": repr(message)}
+
+        # Source https://api-docs.deepseek.com/quick_start/pricing
+        # This will need to be updated if newer models are released.
+        if "DeepSeek-V3" in self.model_name:
+            api_model_name = "deepseek-chat"
+        elif "DeepSeek-R1" in self.model_name:
+            api_model_name = "deepseek-reasoner"
+        else:
+            raise ValueError(f"Model name {self.model_name} not supported by DeepSeek API")
+
+        return self.generate_with_backoff(
+            model=api_model_name,
+            messages=message,
+        )
