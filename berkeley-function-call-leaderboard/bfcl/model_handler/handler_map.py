97f6d6edc003d98fafeec38641738b89758acc68
Huanzhi (Hans) Mao <huanzhimao@gmail.com>
2025-02-05 18:52:35 -0800

    [BFCL] Add New Model `o3-mini-2025-01-31` and `o3-mini-2025-01-31-FC` (#898)
    Add the following new models to the leaderboard:
      - `o3-mini-2025-01-31-FC`
      - `o3-mini-2025-01-31`

--- berkeley-function-call-leaderboard/bfcl/model_handler/handler_map.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/handler_map.py
@@ -43,8 +43,8 @@ api_inference_handler_map = {
     "DeepSeek-V3": DeepSeekAPIHandler,
     "o1-2024-12-17-FC": OpenAIHandler,
     "o1-2024-12-17": OpenAIHandler,
-    # "o1-mini-2024-09-12-FC": OpenAIHandler,  # o1-mini-2024-09-12 does not support function calling
-    "o1-mini-2024-09-12": OpenAIHandler,
+    "o3-mini-2025-01-31-FC": OpenAIHandler,
+    "o3-mini-2025-01-31": OpenAIHandler,
     "gpt-4o-2024-11-20": OpenAIHandler,
     "gpt-4o-2024-11-20-FC": OpenAIHandler,
     "gpt-4o-mini-2024-07-18": OpenAIHandler,
@@ -174,6 +174,7 @@ local_inference_handler_map = {
 outdated_model_handler_map = {
     # "gorilla-openfunctions-v0": GorillaHandler,
     # "o1-preview-2024-09-12": OpenAIHandler,
+    # "o1-mini-2024-09-12": OpenAIHandler,
     # "gpt-4o-2024-08-06": OpenAIHandler,
     # "gpt-4o-2024-08-06-FC": OpenAIHandler,
     # "gpt-4o-2024-05-13": OpenAIHandler,
