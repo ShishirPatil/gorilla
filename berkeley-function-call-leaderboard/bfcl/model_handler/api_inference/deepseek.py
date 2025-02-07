86363d375452f0e9a23191febe46fdb7393f2fb0
Huanzhi (Hans) Mao <huanzhimao@gmail.com>
2025-02-06 16:30:53 -0800

    record reasoning content in result file

--- berkeley-function-call-leaderboard/bfcl/model_handler/api_inference/deepseek.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/api_inference/deepseek.py
@@ -44,7 +44,9 @@ class DeepSeekAPIHandler(OpenAIHandler):
         if "DeepSeek-V3" in self.model_name:
             api_model_name = "deepseek-chat"
         else:
-            raise ValueError(f"Model name {self.model_name} not yet supported in this method")
+            raise ValueError(
+                f"Model name {self.model_name} not yet supported in this method"
+            )
 
         if len(tools) > 0:
             return self.generate_with_backoff(
@@ -64,10 +66,10 @@ class DeepSeekAPIHandler(OpenAIHandler):
     def _query_prompting(self, inference_data: dict):
         """
         This method is intended to be used by the `DeepSeek-R1` models. If used for other models, you will need to modify the code accordingly.
-        
+
         Reasoning models don't support temperature parameter
         https://api-docs.deepseek.com/guides/reasoning_model
-        
+
         `DeepSeek-R1` should use `deepseek-reasoner` as the model name in the API
         https://api-docs.deepseek.com/quick_start/pricing
         """
@@ -77,13 +79,15 @@ class DeepSeekAPIHandler(OpenAIHandler):
         if "DeepSeek-R1" in self.model_name:
             api_model_name = "deepseek-reasoner"
         else:
-            raise ValueError(f"Model name {self.model_name} not yet supported in this method")
+            raise ValueError(
+                f"Model name {self.model_name} not yet supported in this method"
+            )
 
         return self.generate_with_backoff(
             model=api_model_name,
             messages=message,
         )
-        
+
     @override
     def _parse_query_response_prompting(self, api_response: any) -> dict:
         response_data = {
@@ -92,9 +96,9 @@ class DeepSeekAPIHandler(OpenAIHandler):
             "input_token": api_response.usage.prompt_tokens,
             "output_token": api_response.usage.completion_tokens,
         }
-        
+
         # Include reasoning_content only if it exists
         if hasattr(api_response.choices[0].message, "reasoning_content"):
-            response_data["model_responses_reasoning_content"] = api_response.choices[0].message.reasoning_content
+            response_data["reasoning_content"] = api_response.choices[0].message.reasoning_content
         
         return response_data
