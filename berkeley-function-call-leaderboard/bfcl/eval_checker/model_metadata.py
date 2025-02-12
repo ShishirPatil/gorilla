95620b904eec647fcfda85bed7ec60dce3342b78
iamskeole <214513857+iamskeole@users.noreply.github.com>
2025-02-12 00:37:03 -0800

    Update model_metadata.py (#907)
    Co-authored-by: Huanzhi (Hans) Mao <huanzhimao@gmail.com>

--- berkeley-function-call-leaderboard/bfcl/eval_checker/model_metadata.py
+++ berkeley-function-call-leaderboard/bfcl/eval_checker/model_metadata.py
@@ -880,21 +880,21 @@ MODEL_METADATA_MAPPING = {
     "uiuc-convai/CALM-8B": [
         "CALM-8B",
         "https://huggingface.co/uiuc-convai/CALM-8B",
-        "UIUC + Oumi"
-        "Meta Llama 3 Community"
+        "UIUC + Oumi",
+        "Meta Llama 3 Community",
     ],
     "uiuc-convai/CALM-70B": [
-        "CALM-8B",
+        "CALM-70B",
         "https://huggingface.co/uiuc-convai/CALM-70B",
-        "UIUC + Oumi"
-        "Meta Llama 3 Community"
+        "UIUC + Oumi",
+        "Meta Llama 3 Community",
     ],
     "uiuc-convai/CALM-405B": [
-        "CALM-8B",
+        "CALM-405B",
         "https://huggingface.co/uiuc-convai/CALM-405B",
-        "UIUC + Oumi"
-        "Meta Llama 3 Community"
-    ]
+        "UIUC + Oumi",
+        "Meta Llama 3 Community",
+    ],
 }
 
 INPUT_PRICE_PER_MILLION_TOKEN = {
