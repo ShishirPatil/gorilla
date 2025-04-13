02a872e9bfa6a20cc479050e282e6cac51326f98
张可成 <zhangkecheng@mininglamp.com>
2025-04-13 18:20:56 +0800

    Update mining.py: inherit OpenAIHander, and implement our own method

--- berkeley-function-call-leaderboard/bfcl/model_handler/api_inference/mining.py
+++ berkeley-function-call-leaderboard/bfcl/model_handler/api_inference/mining.py
@@ -3,59 +3,39 @@ import os
 import time
 
 import re
-from bfcl.model_handler.base_handler import BaseHandler
-from bfcl.constants.type_mappings import GORILLA_TO_OPENAPI
 from bfcl.model_handler.model_style import ModelStyle
 from bfcl.model_handler.utils import (
-    convert_to_function_call,
-    convert_to_tool,
-    format_execution_results_prompting,
     func_doc_language_specific_pre_processing,
     retry_with_backoff,
 )
 from openai import OpenAI, RateLimitError
+from bfcl.model_handler.api_inference.openai import OpenAIHandler
 
-
-class MiningHandler(BaseHandler):
+class MiningHandler(OpenAIHandler):
     def __init__(self, model_name, temperature) -> None:
         super().__init__(model_name, temperature)
         self.model_style = ModelStyle.OpenAI
         self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"),base_url=os.getenv("OPENAI_API_BASE"))
 
     def decode_ast(self, result, language="Python"):
-        if "FC" in self.model_name or self.is_fc_model:
-            decoded_output = []
-            for invoked_function in result:
-                name = list(invoked_function.keys())[0]
-                params = json.loads(invoked_function[name])
-                decoded_output.append({name: params})
-            return decoded_output
-        else:
-            decoded_output = []
-            for invoked_function in result:
-                name = invoked_function["name"]
-                params = invoked_function["arguments"]
-                decoded_output.append({name: params})
-            return decoded_output
-            # return default_decode_ast_prompting(result, language)
+        decoded_output = []
+        for invoked_function in result:
+            name = invoked_function["name"]
+            params = invoked_function["arguments"]
+            decoded_output.append({name: params})
+        return decoded_output
 
     def decode_execute(self, result):
-        if "FC" in self.model_name or self.is_fc_model:
-            return convert_to_function_call(result)
-        else:
-            # print("qwen_ming:45,decode_execute->",result)
-            # return default_decode_execute_prompting(result)
-            too_call_format = []
-            for tool_call in result:
-                if isinstance(tool_call, dict):
-                    name = tool_call.get("name", "")
-                    arguments = tool_call.get("arguments", {})
-                    args_str = ", ".join(
-                        [f"{key}={repr(value)}" for key, value in arguments.items()]
-                    )
-                    too_call_format.append(f"{name}({args_str})")
-            # print("qwen_ming:45,decode_execute->",too_call_format)
-            return too_call_format
+        too_call_format = []
+        for tool_call in result:
+            if isinstance(tool_call, dict):
+                name = tool_call.get("name", "")
+                arguments = tool_call.get("arguments", {})
+                args_str = ", ".join(
+                    [f"{key}={repr(value)}" for key, value in arguments.items()]
+                )
+                too_call_format.append(f"{name}({args_str})")
+        return too_call_format
 
     @retry_with_backoff(error_type=RateLimitError)
     def generate_with_backoff(self, **kwargs):
@@ -91,64 +71,22 @@ class MiningHandler(BaseHandler):
     def _parse_query_response_prompting(self, api_response: any) -> dict:
         match = re.search(r'<tool_calls>\n(.*?)\n</tool_calls>', api_response.choices[0].message.content, re.DOTALL)
         tool_calls = api_response.choices[0].message.content
-        tool_call_format = []
         # 如果匹配不到则输出原始内容
         if match:
            tool_calls =  match.group(1).strip()
         try:
-        #    calls = eval(calls)
             tool_calls = tool_calls.replace("'",'"')
             tool_calls = json.loads(tool_calls)
-            # for tool_call in tool_calls:
-            #     tool_name = tool_call.get("name","")
-            #     arguments = tool_call.get("arguments","")
-
-            #     tool_call_format.append()
         except:
             pass
         message = api_response.choices[0].message
-        # message = [x.replace("'",'"') for x in message]
-        # print("qwen_mining:209,->",calls)
         return {
-            # "model_responses": api_response.choices[0].message.content,
             "model_responses": tool_calls,
             "model_responses_message_for_chat_history": message,
             "input_token": api_response.usage.prompt_tokens,
             "output_token": api_response.usage.completion_tokens,
         }
 
-    def add_first_turn_message_prompting(
-        self, inference_data: dict, first_turn_message: list[dict]
-    ) -> dict:
-        # print("qwen_mining:222,add_first_turn_message_prompting->",first_turn_message)
-        inference_data["message"].extend(first_turn_message)
-        return inference_data
-
-    def _add_next_turn_user_message_prompting(
-        self, inference_data: dict, user_message: list[dict]
-    ) -> dict:
-        inference_data["message"].extend(user_message)
-        return inference_data
-
-    def _add_assistant_message_prompting(
-        self, inference_data: dict, model_response_data: dict
-    ) -> dict:
-        inference_data["message"].append(
-            model_response_data["model_responses_message_for_chat_history"]
-        )
-        return inference_data
-
-    def _add_execution_results_prompting(
-        self, inference_data: dict, execution_results: list[str], model_response_data: dict
-    ) -> dict:
-        formatted_results_message = format_execution_results_prompting(
-            inference_data, execution_results, model_response_data
-        )
-        inference_data["message"].append(
-            {"role": "user", "content": formatted_results_message}
-        )
-
-        return inference_data
     def mining_system_prompt_pre_processing_chat_model(self,prompts, function_docs, test_category):
         system_pre="""You are a function calling AI model. 
 You are provided with function signatures within <tools></tools> XML tags.
