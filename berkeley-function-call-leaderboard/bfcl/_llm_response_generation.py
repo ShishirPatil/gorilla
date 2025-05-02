ShishirPatil/gorilla

commit 60c26b2c90163971e30b184562c1f3d5105c666c

[BFCL] Add Validation for Model Names (#1008)
Throw clear error messages when model name is unknown/invalid.

berkeley-function-call-leaderboard/bfcl/_llm_response_generation.py
@@ -276,6 +276,13 @@ def main(args):
         all_test_entries_involved,
     ) = get_involved_test_entries(args.test_category, args.run_ids)
 
+    for model_name in args.model:
+        if model_name not in HANDLER_MAP:
+            raise ValueError(
+                        f"Unknown model_name '{model_name}'.\n"
+                        "• For officially supported models, please refer to `SUPPORTED_MODELS.md`.\n"
+                        "• For running new models, please refer to `README.md` and `CONTRIBUTING.md`."
+                    )
     print(f"Generating results for {args.model}")
     if args.run_ids:
         print("Running specific test cases. Ignoring `--test-category` argument.")

berkeley-function-call-leaderboard/bfcl/eval_checker/eval_runner.py
@@ -488,6 +488,8 @@ def main(model, test_categories, result_dir, score_dir):
     if model:
         model_names = []
         for model_name in model:
+            if model_name not in HANDLER_MAP:
+                raise ValueError(f"Invalid model name '{model_name}'.")
             # Runner takes in the model name that contains "_", instead of "/", for the sake of file path issues.
             # This is differnet than the model name format that the generation script "openfunctions_evaluation.py" takes in (where the name contains "/").
             # We patch it here to avoid confusing the user.

commit 56d7a7c172ddbce25c1b9a6bd64acca7ed75063e

[BFCL] Update Error Message for New Handler Mappings (#1013)
Bugfix for an issure introduced in #1008

berkeley-function-call-leaderboard/bfcl/_llm_response_generation.py
@@ -277,7 +277,7 @@ def main(args):
     ) = get_involved_test_entries(args.test_category, args.run_ids)
 
     for model_name in args.model:
-        if model_name not in HANDLER_MAP:
+        if model_name not in MODEL_CONFIG_MAPPING:
             raise ValueError(
                         f"Unknown model_name '{model_name}'.\n"
                         "• For officially supported models, please refer to `SUPPORTED_MODELS.md`.\n"

berkeley-function-call-leaderboard/bfcl/eval_checker/eval_runner.py
@@ -488,7 +488,7 @@ def main(model, test_categories, result_dir, score_dir):
     if model:
         model_names = []
         for model_name in model:
-            if model_name not in HANDLER_MAP:
+            if model_name not in MODEL_CONFIG_MAPPING:
                 raise ValueError(f"Invalid model name '{model_name}'.")
             # Runner takes in the model name that contains "_", instead of "/", for the sake of file path issues.
             # This is differnet than the model name format that the generation script "openfunctions_evaluation.py" takes in (where the name contains "/").