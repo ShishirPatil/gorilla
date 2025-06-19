ab88888bb4f1a055e2edeb0e3273a33980921fc1
snova-jorgep <jorge.piedrahita@sambanovasystems.com>
2025-06-19 15:47:49 -0500

    fix concurrency

--- berkeley-function-call-leaderboard/test_openai_providers.py
+++ berkeley-function-call-leaderboard/test_openai_providers.py
@@ -10,7 +10,8 @@ import json
 import time
 import csv
 import os
-
+from pathlib import Path
+import pandas as pd
 
 load_dotenv()
 
@@ -28,7 +29,9 @@ CSV_HEADERS = [
 ]
 
 date = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-with open(f'./scores-{date}.csv', 'w', newline='') as csvfile:
+csv_file_path = f'./scores-{date}.csv'
+csv_file_path, Path(csv_file_path).parent.mkdir(parents=True, exist_ok=True)
+with open(csv_file_path, 'w', newline='') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=CSV_HEADERS, delimiter=';')
     writer.writeheader()
 
@@ -37,6 +40,10 @@ def load_json(file_path):
         return json.load(f)
 
 def run_bfcl_command(command_type, model, test_category, result_dir, score_dir=None):
+    
+    result_dir = str(result_dir)
+    score_dir = str(score_dir)
+    
     if command_type not in ["generate", "evaluate"]:
         raise ValueError("Invalid command_type. Must be 'generate' or 'evaluate'.")
 
@@ -60,60 +67,83 @@ def run_bfcl_command(command_type, model, test_category, result_dir, score_dir=N
         print(f"FAILED [{model}] - Return code: {e.returncode}")
         print("Error output:", e.stderr)
 
-def run_models_for_provider(provider, models, run_id, test_category, model_map, lock, date,
-                             concurrency_models: bool = True):
-    result_path = os.path.join("result", provider, date)
-    score_path = os.path.join("score", provider, date)
-
+def run_models_for_provider(provider, 
+                            models,
+                            run_id,
+                            test_category,
+                            model_map,
+                            lock,
+                            date,
+                            concurrency_models: bool = True
+                            ):
     
-    def run_model(model):
-        run_bfcl_command("generate", model, ",".join(test_category), result_path)
-        run_bfcl_command("evaluate", model, ",".join(test_category), result_path, score_dir=score_path)
+    result_path = Path("./result") / provider / date
+    score_path = Path("./score") / provider / date
 
-    if concurrency_models is True:
-    # Run all models concurrently
-        with concurrent.futures.ThreadPoolExecutor() as model_executor:
-            model_futures = [model_executor.submit(run_model, model) for model in models]
-            concurrent.futures.wait(model_futures)
-    else:
-        for model in models:
-            run_bfcl_command("generate", model, ",".join(test_category), result_path)
-            run_bfcl_command("evaluate", model, ",".join(test_category), result_path, score_dir=score_path)
-
-    for category in test_category:
-        curr_dict = {
-            "run_id": run_id,
-            "test_suite_name": f"Berkeley AI Benchmarking - {category}",
-            "provider": provider,
-            "date": date
-        }
-
-        for model in models:
+    def run_model(model, concurrent):
+        for category in test_category:
+            run_bfcl_command("generate", model, category, result_path)
+            run_bfcl_command("evaluate", model, category, result_path, score_dir=score_path)
+            curr_dict = {
+                "run_id": run_id,
+                "test_suite_name": f"Berkeley AI Benchmarking - {category}",
+                "provider": provider,
+                "date": date
+            }
             model_safe = model.replace("/", "_")
-            score_path = os.path.join("score", provider, date, model_safe, f"BFCL_v3_{category}_score.json")
-
+            category_score_path = score_path / model_safe / f"BFCL_v3_{category}_score.json"
+            
             try:
-                with open(score_path, "r") as f:
+                with open(category_score_path, "r") as f:
                     first_line = f.readline()
                     data = json.loads(first_line)
                     curr_dict["n_samples"] = data.get("total_count")
                     curr_dict[model_map[model_safe]] = data.get("accuracy")
+                print(f"inserting data: {curr_dict}")
             except FileNotFoundError:
-                print(f"Score file not found: {score_path}")
+                print(f"Score file not found: {category_score_path}")
             except json.JSONDecodeError:
-                print(f"Invalid JSON in file: {score_path}")
+                print(f"Invalid JSON in file: {category_score_path}")
             except Exception as e:
                 print(f"Error reading score for {provider}/{model}: {e}")
 
-        with lock:
-            csv_file_path = f'./scores-{date}.csv'
-            Path(csv_file_path).parent.mkdir(parents=True, exist_ok=True)
-            with open(csv_file_path, 'a', newline='') as csvfile:
-                writer = csv.DictWriter(csvfile, fieldnames=CSV_HEADERS, delimiter=';', extrasaction='ignore')
-                if os.stat(csv_file_path).st_size == 0:
-                    writer.writeheader()
-                writer.writerow(curr_dict)
-            upload_to_s3(csv_file_path, f"fc-so-testing-suite/gorilla-snova/{PurePosixPath(csv_file_path).name}")
+            if concurrent:
+                with lock:
+                    df =  pd.read_csv(csv_file_path, sep=";")
+                    mask = (
+                        (df["test_suite_name"] == curr_dict["test_suite_name"]) &
+                        (df["provider"] == curr_dict["provider"])
+                    )
+                    if df[mask].empty:
+                        df = pd.concat([df, pd.DataFrame([curr_dict])], ignore_index=True)
+                    else:
+                        for key, value in curr_dict.items():
+                            df.loc[mask, key] = value
+                    df.to_csv(csv_file_path, index=False, sep=";")
+                    upload_to_s3(csv_file_path, f"fc-so-testing-suite/gorilla-snova/{PurePosixPath(csv_file_path).name}")
+            else:
+                df =  pd.read_csv(csv_file_path, sep=";")
+                mask = (
+                    (df["test_suite_name"] == curr_dict["test_suite_name"]) &
+                    (df["provider"] == curr_dict["provider"])
+                )
+                if df[mask].empty:
+                    df = pd.concat([df, pd.DataFrame([curr_dict])], ignore_index=True)
+                else:
+                    for key, value in curr_dict.items():
+                        df.loc[mask, key] = value
+                df.to_csv(csv_file_path, index=False, sep=";")
+                upload_to_s3(csv_file_path, f"fc-so-testing-suite/gorilla-snova/{PurePosixPath(csv_file_path).name}")
+        
+
+    if concurrency_models is True:
+    # Run all models concurrently
+        with concurrent.futures.ThreadPoolExecutor() as model_executor:
+            model_futures = [model_executor.submit(run_model, model, concurrency_models) for model in models]
+            concurrent.futures.wait(model_futures)
+    else:
+        for model in models:
+            run_model(model, concurrency_models)
 
 def main(date: str):
     json_path = "provider_models.json"
@@ -124,7 +154,7 @@ def main(date: str):
     run_id = str(uuid.uuid4())
     lock = Lock()
 
-    subset_command = ["python", "generate_subsets.py", "BFCL_v3_simple", "BFCL_v3_live_simple", "BFCL_v3_multi_turn_base", "BFCL_v3_multiple", "BFCL_v3_live_multiple", "BFCL_v3_parallel_multiple", "BFCL_v3_live_parallel_multiple", "BFCL_v3_multi_turn_long_context", "-n", "5"]
+    subset_command = ["python", "generate_subsets.py", "BFCL_v3_simple", "BFCL_v3_live_simple", "BFCL_v3_multi_turn_base", "BFCL_v3_multiple", "BFCL_v3_live_multiple", "BFCL_v3_parallel_multiple", "BFCL_v3_live_parallel_multiple", "BFCL_v3_multi_turn_long_context", "-n", "20"]
 
     try:
         print(f"\nRunning subset generation: {' '.join(subset_command)}")
