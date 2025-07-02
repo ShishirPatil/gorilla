88e1c2c0814e251b675afce8cda8fa9c08dd16e5
Apollo Loh <yihhang@gmail.com>
2025-07-02 16:14:42 -0700

    Only newer Responses handler should have developer role

--- berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/openai_response.py
+++ berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/openai_response.py
@@ -10,10 +10,10 @@ from bfcl_eval.model_handler.utils import (
     convert_to_tool,
     default_decode_ast_prompting,
     default_decode_execute_prompting,
+    developer_prompt_pre_processing_chat_model,
     format_execution_results_prompting,
     func_doc_language_specific_pre_processing,
     retry_with_backoff,
-    system_prompt_pre_processing_chat_model,
 )
 from openai import OpenAI, RateLimitError
 from openai.types.responses import Response
@@ -186,7 +186,7 @@ class OpenAIResponsesHandler(BaseHandler):
 
         functions = func_doc_language_specific_pre_processing(functions, test_category)
 
-        test_entry["question"][0] = system_prompt_pre_processing_chat_model(
+        test_entry["question"][0] = developer_prompt_pre_processing_chat_model(
             test_entry["question"][0], functions, test_category
         )
 
