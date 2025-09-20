ab46d58db5d31fed24ea479d6fb11465bfc6473a
zhaoshengbo-source <zhaoshengbo@berkeley.edu>
2025-09-20 10:25:22 -0700

    Update mistral.py
    Pass the underscore_to_dot flag into the _compile_tools method

--- berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/mistral.py
+++ berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/mistral.py
@@ -75,7 +75,7 @@ class MistralHandler(BaseHandler):
     def _compile_tools(self, inference_data: dict, test_entry: dict) -> dict:
         functions: list = test_entry["function"]
 
-        tools = convert_to_tool(functions, GORILLA_TO_OPENAPI, self.model_style)
+        tools = convert_to_tool(functions, GORILLA_TO_OPENAPI, self.model_style, underscore_to_dot=getattr(self, "underscore_to_dot",False))
 
         inference_data["tools"] = tools
 
