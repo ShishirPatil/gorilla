8c085049cf6c747bacbe4a122820d4f1d6b97659
iamskeole <214513857+iamskeole@users.noreply.github.com>
2025-10-27 01:07:00 -0700

    [BFCL] Add DeepSeek-V3.2-Exp， Claude 4.5 Sonnet and 4.5 Haiku model support (#1205)
    This pull request adds support for the following new models:
    
    - DeepSeek-V3.2-Exp
    - Claude 4.5 Sonnet 
    - Claude 4.5 Haiku
    
    ---------
    
    Co-authored-by: Huanzhi Mao <huanzhimao@gmail.com>

--- berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/claude.py
+++ berkeley-function-call-leaderboard/bfcl_eval/model_handler/api_inference/claude.py
@@ -69,9 +69,9 @@ class ClaudeHandler(BaseHandler):
         """
         if "claude-opus-4-1-20250805" in self.model_name:
             return 32000
-        elif "claude-sonnet-4-20250514" in self.model_name:
+        elif "claude-sonnet-4-5-20250929" in self.model_name:  
             return 64000
-        elif "claude-3-5-haiku-20241022" in self.model_name:
+        elif "claude-haiku-4-5-20251001" in self.model_name:
             return 8192
         else:
             raise ValueError(f"Unsupported model: {self.model_name}")
