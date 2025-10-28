c4b0ea0b4ba7e79888d786be6c1e029427fae5ab
Joseph Tennyson <jmtennyson7@gmail.com>
2025-10-28 13:46:51 -0700

    message api incosistency with delete and text

--- berkeley-function-call-leaderboard/bfcl_eval/eval_checker/multi_turn_eval/func_source_code/message_api.py
+++ berkeley-function-call-leaderboard/bfcl_eval/eval_checker/multi_turn_eval/func_source_code/message_api.py
@@ -216,7 +216,7 @@ class MessageAPI:
                 return {
                     "deleted_status": True,
                     "message_id": receiver,
-                    "message": f"Receiver {receiver_id}'s first message deleted successfully.",
+                    "message": f"Receiver {receiver_id}'s latest message deleted successfully.",
                 }
         return {"error": f"Receiver ID {receiver_id} not found."}
 
